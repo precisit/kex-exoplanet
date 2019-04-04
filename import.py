13e93b3407446e895105cf6c5e200c83e42469d2
fredrikorn <48676646+fredrikorn@users.noreply.github.com>
2019-04-04 13:54:41 +0200

    import.py now reads data, trains a simple SVC and plots a flux-curve

--- import.py
+++ import.py
@@ -1,12 +1,13 @@
 #Trying out code from @gabogarza
 
 #Requires training and test data in a folder named datasets
-print( __name__)
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
-print( __name__)
+
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
 def np_X_Y_from_df(df):
     print("In np_x_y...")
@@ -23,12 +24,38 @@ def main():
     df_train = pd.read_csv(trainSetPath, encoding = "ISO-8859-1")
     df_dev = pd.read_csv(testSetPath, encoding = "ISO-8859-1")
 
+    print(df_train.info())
+
     # Generate X and Y dataframe sets
     df_train_x = df_train.drop('LABEL', axis=1)
     df_dev_x = df_dev.drop('LABEL', axis=1)
     df_train_y = df_train.LABEL
     df_dev_y = df_dev.LABEL
     X = np.array(df_train_x)
+    Y = np.array(df_train_y)
+    Xd= np.array(df_dev_x)
+    Yd= np.array(df_dev_y)
+    
+
+    model=LinearSVC()
+    print("Training...")
+    model.fit(X, Y)
+    print("Finished training!")
+    train_outputs=model.predict(X)
+    test_outputs =model.predict(Xd)
+    acc=accuracy_score(Y,train_outputs)
+    recall_train = recall_score(Y, train_outputs)
+    confM=confusion_matrix(Y,train_outputs)
+    print(acc)
+    print(recall_train)
+    print("Confusion Matrix - Train Set")
+    print(confM)
+
+    confMd=confusion_matrix(Yd,test_outputs)
+    print("Confusion Matrix - Test Set")
+    print(confMd)
+
+    #PLOTTING
     plt.plot(X[1, :], '.')
     plt.show()
 
