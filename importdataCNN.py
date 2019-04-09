d43bdecbe91de0e291112b212e694d70df980166
Maja Linderholm <maja.linderholm@gmail.com>
2019-04-09 15:41:36 +0200

    changed some variables and comments

--- importdataCNN.py
+++ importdataCNN.py
@@ -1,47 +1,44 @@
-# Program that imports and preprocess our data to meet 
-# requirments that CNN have on input data.
+# PROGRAM THAT IMPORTS AND PREPROCESSES DATA TO MEET
+# REQUIRMENTS THAT CNN HAVE ON INPUT DATA
 
+# IMPORT PACKAGES
 import pandas as pd
 import numpy as np
-
-from scipy.ndimage.filters import uniform_filter1d
-
 import matplotlib.pyplot as plt
+from scipy.ndimage.filters import uniform_filter1d
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
-
+# DEFINE MAIN-FUNCTION
 def main():
-    trainSetPath = "datasets/exoTrain.csv"
+    trainSetPath = "datasets/exoTrain.csv" #define path for data files
     testSetPath = "datasets/exoTest.csv"
     print("Loading datasets...")
-    df_train = pd.read_csv(trainSetPath, encoding= "ISO-8859-1")
-    df_dev = pd.read_csv(testSetPath, encoding= "ISO-8859-1")
-    
-    print(df_train.info())
+    train = pd.read_csv(trainSetPath, encoding= "ISO-8859-1") #on data frame format
+    test = pd.read_csv(testSetPath, encoding= "ISO-8859-1") #on data frame format
+    print(train.info())
 
-    # Converting the pandas dataframe to numpy arrays (matrices)
-    df_train_x = df_train.drop('LABEL', axis=1)
-    df_test_x = df_dev.drop('LABEL', axis=1)
-    df_train_y = df_train.LABEL
-    df_test_y = df_dev.LABEL
-    X_train = np.array(df_train_x)
-    Y_train = np.array(df_train_y)    
-    X_test = np.array(df_test_x)
-    Y_test = np.array(df_test_y)
+    # CONVERTING THE PANDAS DATAFRAM TO NUMPY ARRAYS (MATRICES)
+    train_x = train.drop('LABEL', axis=1) #removes "label" from x-values
+    test_x = test.drop('LABEL', axis=1) 
+    train_y = train.LABEL #picks "label" to y-values
+    test_y = test.LABEL
+    x_train = np.array(train_x) #changes format form data frame to numpy array
+    y_train = np.array(train_y)
+    x_test = np.array(test_x) 
+    y_test = np.array(test_y)
 
-    # Scale each observation to zeromean and unit variance
+    # SCALE EACH OBSERVATION TO ZERO MEAN AND UNIT VARIANCE
+    x_train = ((x_train - np.mean(x_train, axis=1).reshape(-1,1)) / np.std(x_train, axis=1).reshape(-1,1))
+    x_test = ((x_test - np.mean(x_test, axis=1).reshape(-1,1)) / np.std(x_test, axis=1).reshape(-1,1))
 
-    X_train1 = ((X_train - np.mean(X_train, axis=1).reshape(-1,1)) / np.std(X_train, axis=1).reshape(-1,1))
-    X_test1 = ((X_test - np.mean(X_test, axis=1).reshape(-1,1)) / np.std(X_test, axis=1).reshape(-1,1))
-
-    #PLOTTING the first processed light curve
+    # PLOTTING THE FIRST PROCESSED LIGHT CURVE
     plt.subplot(2, 1, 1)
-    plt.plot(X_train[1, :], '.')
+    plt.plot(x_train[1, :], '.')
     plt.title('Unprocessed light curve')
 
-    #Plotting the second processed light curve
+    # PLOTTING THE SECOND PROCESSED LIGHT CURVE
     plt.subplot(2, 1, 2)
-    plt.plot(X_train1[1, :], '.')
+    plt.plot(X_test1[1, :], '.')
     plt.title('Processed light curve')
     plt.show()
 
