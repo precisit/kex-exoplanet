088662f59e249cbf9d914cc254287f997eb014b8
Maja Linderholm <maja.linderholm@gmail.com>
2019-04-16 11:19:41 +0200

    added some more lines of code to the batch generator

--- importdataCNN.py
+++ importdataCNN.py
@@ -52,6 +52,46 @@ def main():
     # DEFINE THE NEURAL NETWORK
     model = Sequential()
     model.add(Conv1D(filters=8, kernel_size=11, activation='relu', input_shape=x_train.shape[1:]))
+    model.add(MaxPool1D(strides=4))
+    model.add(BatchNormalization())
+    model.add(Conv1D(filters=16, kernel_size=11, activation='relu'))
+    model.add(MaxPool1D(strides=4))
+    model.add(BatchNormalization())
+    model.add(Conv1D(filters=32, kernel_size=11, activation='relu'))
+    model.add(MaxPool1D(strides=4))
+    model.add(BatchNormalization())
+    model.add(Conv1D(filters=64, kernel_size=11, activation='relu'))
+    model.add(MaxPool1D(strides=4))
+    model.add(Flatten())
+    model.add(Dropout(0.5)) #prevents overfitting
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.25)) #prevents overfitting
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # DEFINE FUNCTION THAT GENERATES BATCH WITH EQUALLY POSITIV
+    # AND NEGAIVE SAMPLES, AND ROTATES THEM RANDOMLY IN TIME
+    def batch_generator(x_train, y_train, batch_size=32):
+        half_batch = batch_size // 2
+        x_batch = np.empty((batch_size, x_train.shape[1], x_train.shape[2]), dtype='float32') #define empty batch for input
+        y_batch = np.empty((batch_size, y_train.shape[1]), dtype='float32') #define empty batch for output
+
+        yes_idx = np.where(y_train[:,0] == 2.)[0] 
+        non_idx = np.where(y_train[:,0] == 1.)[0]
+
+        while True:
+            np.random.shuffle(yes_idx)
+            np.random.shuffle(non_idx)
+            x_batch[:half_batch] = x_train[yes_idx[:half_batch]]
+            x_batch[half_batch:] = x_train[non_idx[half_batch:batch_size]]
+            y_batch[:half_batch] = y_train[yes_idx[:half_batch]]
+            y_batch[half_batch:] = y_train[non_idx[half_batch:batch_size]]
+            for i in range(batch_size):
+                sz = np.random.randint(x_batch.shape[1])
+                x_batch[i] = np.roll(x_batch[i], sz, axis = 0)
+     
+            yield x_batch, y_batch
+        
 
 print("Before main")
 if __name__ == '__main__':
